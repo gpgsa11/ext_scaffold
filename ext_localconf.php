e70f4f6d35d9268e4f98e11e73c8d677f6275295
BogdanWGA <bogdan.typo3.wga@gmail.com>
2022-06-08 15:10:46 +0300

    green project

---
+++ ext_localconf.php
@@ -0,0 +1,4 @@
+<?php
+defined('TYPO3_MODE') || die();
+\FluidTYPO3\Flux\Core::registerProviderExtensionKey('Helhum.ExtScaffold', 'Page');
+\FluidTYPO3\Flux\Core::registerProviderExtensionKey('Helhum.ExtScaffold', 'Content');
---
+++ ext_tables.php
@@ -0,0 +1,3 @@
+<?php
+defined('TYPO3_MODE') || die();
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('ext_scaffold', 'Configuration/TypoScript', 'Main provider extension for Rocket Site');
